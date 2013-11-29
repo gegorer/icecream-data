dbc798708f1d9a1447e20059e8968ad85dd0b927
clark_chung <clark_chung@htc.com>
2013-11-29 14:27:54 +0800

    1. Modify data with version added. 2. Add a tool for converting from json to sqlite. 3. Update fields description.

---
+++ convertJsonToSqlite.py
@@ -0,0 +1,49 @@
+import sqlite3
+import json
+
+def main():
+    allShopInfo = json.load(open('all-shop.json'))
+
+    con = sqlite3.connect('all-shop.sqlite')
+    cur = con.cursor()
+    cur.execute('''DROP TABLE IF EXISTS `info`''')
+    cur.execute('''CREATE TABLE `info` (`version` TEXT)''')
+    cur.execute('''INSERT INTO `info` (`version`) VALUES (?)''', (allShopInfo['version'],))
+    fieldnames = allShopInfo['shopList'][0].keys()
+    shopTableCreateStatement = []
+    shopTableCreateStatement.append('CREATE TABLE `shops` (')
+    typeMapping = {'px': 'REAL', 'py': 'REAL'}
+    prefix = ""
+    for fieldname in fieldnames:
+        if fieldname[0].isalpha() and fieldname.isalnum():
+            shopTableCreateStatement.append(prefix + '`' + fieldname + '` ' + typeMapping.get(fieldname, 'TEXT'))
+            prefix = ", "
+    shopTableCreateStatement.append(')')
+    cur.execute('DROP TABLE IF EXISTS `shops`')
+    cur.execute(''.join(shopTableCreateStatement))
+
+    shopInsertData = []
+    for shop in allShopInfo['shopList']:
+        shopData = []
+        for fieldname in fieldnames:
+            shopData.append(shop.get(fieldname, ''))
+        shopInsertData.append(tuple(shopData))
+
+    shopTableInsertStatement = []
+    shopTableInsertStatement.append('INSERT INTO `shops` (')
+    prefix = ""
+    for fieldname in fieldnames:
+        shopTableInsertStatement.append(prefix + '`' + fieldname + '`')
+        prefix = ", "
+    shopTableInsertStatement.append(') VALUES (')
+    shopTableInsertStatement.append('?' + ' ,?' * (len(fieldnames) - 1))
+    shopTableInsertStatement.append(')')
+
+    cur.executemany(''.join(shopTableInsertStatement), shopInsertData)
+
+    cur.close()
+    con.commit()
+    con.close()
+
+if __name__ == '__main__':
+    main()
